e26875de866d4d2798285e5325de524b701d2bc8
pedrogstrindade <dev.pedrogstrindade@gmail.com>
2026-02-03 16:42:10 -0300

    Adicionando Spring Boot ao projeto

---
+++ src/main/java/br/com/tabela_tarifaria_api/TabelaTarifariaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.tabela_tarifaria_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabelaTarifariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabelaTarifariaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tabela_tarifaria_api/TabelaTarifariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tabela_tarifaria_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabelaTarifariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
